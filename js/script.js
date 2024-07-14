91e133b3db12483e4d3660bca85b5b7bb515be33
Abhishek Kumar <154538311+abhi231210003@users.noreply.github.com>
2024-07-14 19:36:16 +0530

    changes in js

--- js/script.js
+++ js/script.js
@@ -70,7 +70,7 @@ const playMusic = (track, pause = false) => {
 };
 
 async function displayAlbums() {
-  let a = await fetch(`songs/`);
+  let a = await fetch(`${window.location.origin}/songs/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
@@ -82,7 +82,7 @@ async function displayAlbums() {
     if (e.href.includes("/songs/") && !e.href.includes()) {
       let folder = e.href.split("/").slice(-1)[0];
       //get meta data of folder
-      let a = await fetch(`/songs/${folder}/info.json`);
+      let a = await fetch(`${window.location.origin}/songs/${folder}/info.json`);
       let response = await a.json();
       cardContainer.innerHTML =cardContainer.innerHTML +
         `<div data-folder="${folder}" class="card">
